ad3beee5160cd76310cea3658360a2ced6cda69e
Breno Vergilio <bjvergilio47@gmail.com>
2022-05-03 09:47:51 -0400

    feat: implemented getVehicleByIDUseCase

--- src/domain/vehicle/VehicleRepository.ts
+++ src/domain/vehicle/VehicleRepository.ts
@@ -3,6 +3,6 @@ import { Vehicle, VehicleStatus } from "./Vehicle";
 export default interface VehicleRepository {
   getAll(): Promise<Vehicle[]>;
   getByStatus(status: VehicleStatus): Promise<Vehicle[]>;
-  getByID(): Promise<Vehicle>;
+  getByID(id: string): Promise<Vehicle>;
   create(newVehicle: Vehicle): Promise<Vehicle>;
 }
---
+++ src/useCases/vehicleRelated/GetVehicleByIDUseCase.ts
@@ -0,0 +1,12 @@
+import NotFoundError from "../errors/NotFoundError";
+import BaseVehicleRelatedUseCase from "./BaseVehicleRelatedUseCase";
+import { InputGetVehicleByID, OutputVehicle } from "./VehicleIO";
+
+export default class GetVehicleByID extends BaseVehicleRelatedUseCase {
+  async handle(inputGetVehicleByID: InputGetVehicleByID): Promise<OutputVehicle> {
+    const vehicle = await this.vehicleRepository.getByID(inputGetVehicleByID.id);
+    if(!vehicle) throw new NotFoundError();
+    const outputVehicle: OutputVehicle = { id: vehicle.id, brand: vehicle.brand, model: vehicle.model, year: vehicle.year, km: vehicle.km, color: vehicle.color, chassi: vehicle.chassi, price: vehicle.price }; 
+    return outputVehicle;
+  }
+}
