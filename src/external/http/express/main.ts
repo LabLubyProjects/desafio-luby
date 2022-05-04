c33df57640a99300a39e9bd4862f0690b56380ab
Breno Vergilio <bjvergilio47@gmail.com>
2022-05-04 13:12:50 -0400

    feat: graceful shutdown

--- src/external/http/express/main.ts
+++ src/external/http/express/main.ts
@@ -1,5 +1,33 @@
 import { App } from './app';
+import { db } from '@src/external/database/sequelize/database';
 
-new App().server.listen(3000, () =>
-  console.log('[EXPRESS SERVER RUNNING ON PORT 3000]')
-);
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+(async (): Promise<void> => {
+  try {
+    await db.authenticate();
+    console.log("[Connected to Database]")
+
+    new App().server.listen(3000, () =>
+      console.log('[EXPRESS SERVER RUNNING ON PORT 3000]')
+    );
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          db.close();
+          console.log('[Database Conection Closed]');
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    process.exit(ExitStatus.Failure);
+  }
+})();
